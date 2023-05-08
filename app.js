06caaea92d0d3b13aa39c66e284758608c101ef4
unknown <asiframzan2021@gmail.com>
2023-05-08 09:05:27 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(async function(e){
+    const api = 'https://fakestoreapi.com/products';
+    const response = await fetch(api);
+    const datas = await response.json();
+    console.log(datas[0]);
+    datas[0].foreach((i)=>{
+        loadProduct(i)
+    })
+})
+function loadProduct(data){
+    var li = document.createElement('li');
+    li.innerHTML = `<div class="card mt-1">` +
+    `<div class="product-1 align-item-center p-2 text-center"> <img src="`+data.image+`" class="rounded" width="150" height="100">` +
+    `<h6 class="mt-0 font-weight-bold mb-2 info">`+data.title+` </h6>` +
+    `<div class="mt-3 info"> <span class="text1 d-block"> `+data.description+` </span></div>`+
+    `<div class="cost mt-1 text-dark"> <span> `+data.price+` </span>`+`</div>` +
+    `</div>` +
+    `<div class="button-color p-2 text-center text-white"> <span class="text-uppercase"> add to cart</span> </div>` + `</div>`
+    document.getElementById('home_product').appendChild(li);
+
+}
