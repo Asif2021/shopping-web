4d5f2e086a8c3c62bb2b1836ea92aaf68278071a
unknown <asiframzan2021@gmail.com>
2023-05-20 15:23:36 +0500

    card styled

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
-$(document).ready(async function(e){
-    // hittiing a api 
-    const api = 'https://fakestoreapi.com/products';
-    // getting response from api 
-    const response = await fetch(api);
-    // getting data from in json format 
-    const datas = await response.json();
-    console.log(datas);
-    
-    datas.map((i)=>{
-        // calling function loadProduct and passing data as i 
-        loadProduct(i);
+
+    // hitting a api 
+    fetch("https://fakestoreapi.com/products").then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        console.log(data);
+        let product = "";
+        data.map((value)=>{
+product += `<div class="card col-10 "">
+   <img class="card-img-top img" src="${value.image}" alt="Card image">
+     <div class="card-body d-flex flex-column">
+       <h5 class="card-title">${value.title}</h5>
+    <p class="card-text">${value.price}</p>
+     <button class="button mt-auto" onclick="${add(value)}">add to cart </button>
+     </div>
+   </div>`
+        })
+        document.getElementById("home_products").innerHTML = product;
     })
-   })
+  
+    
+    
    
-// loadProduct function defination 
-function loadProduct(data){
-    var li = document.createElement('li');
-    li.innerHTML = `<div class="card">` +
-    `<div class="product-1 align-item-center p-2 text-center"> <img src="`+data.image+`" class="rounded" width="150" height="100">` +
-    `<h6 class="m-2 font-weight-bold info">`+data.title+` </h6>` +
-    `<div class="mt-3"> <span class=" text-1 d-block"> `+data.description+` </span></div>`+
-    `<div class="cost mt-1 text-dark"> <span> `+data.price+` </span>`+`</div>` +
-    `</div>` +
-    `<button class="button-color p-2 text-center text-white" onclick="add(`+(data.price)+`)"> Add to Cart</button>` + `</div>`
-    document.getElementById("home_products").appendChild(li);
-
-}
-
+   
 
 var totalItems = 0;
 document.getElementById('totalCost').innerText = totalItems;
 
 // adding items into cart 
 function add(val){
-    document.getElementById("costList").innerHTML += "<li class='liID'>" +val+ `<button onclick="remove()"> X </button> `+"</li>";
+
+    document.getElementById("product_table").innerHTML += `<tbody>
+    <tr>
+      <th scope="row">${val.title}</th>
+      <td>${val.price}</td>
+      <td><img class="table_image" src="${val.image}" alt="image"></td>
+      <td><i class="fa fa-trash" aria-hidden="true"></i></td>
+    </tr>
+    </tbody>`
     totalItems = totalItems+1;
     document.getElementById('totalCost').innerText = totalItems;
-
 }
 
 // removing item from cart 
